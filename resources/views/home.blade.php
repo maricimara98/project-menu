846c11badaad59e1e4c779f5f71087b785507d5c
Jucimara Pereira <maricimara98@outlook.com>
2022-11-04 12:09:16 -0300

    add rota e contoller

---
+++ resources/views/home.blade.php
@@ -0,0 +1,61 @@
+@extends('layouts.app')
+@section('menu')
+    <div class="topo">
+        <div class="menubtn">
+            <a href="#"><i class="fa-solid fa-bars"></i></a>
+        </div>
+    </div>
+
+    <nav class="sidebar">
+        <div class="text">Menu</div>
+        <ul id="ul-principal">
+            <div>
+                <li class="li-p">
+                    <a href="#" class="dropbtn" onclick="opc1()">Opc 1
+                        <span class="fas fa-caret-down"></span>
+                    </a>
+                    <ul id="myDropdown1" class="dropdown-content">
+                        <li><a href="#">intro 1</a></li>
+                        <li><a href="#">intro 2</a></li>
+                    </ul>
+                </li>
+            </div>
+            <div>
+                <li class="li-p">
+                    <a href="#" class="dropbtn" onclick="opc2()">Opc 2
+                        <span class="fas fa-caret-down"></span>
+                    </a>
+                    <ul id="myDropdown2" class="dropdown-content">
+                        <li><a href="#">intro 1</a></li>
+                        <li><a href="#">intro 2</a></li>
+                    </ul>
+                </li>
+            </div>
+            <div>
+                <li class="li-p">
+                    <a href="#" class="dropbtn" onclick="opc3()">Opc 3
+                        <span class="fas fa-caret-down"></span>
+                    </a>
+                    <ul id="myDropdown3" class="dropdown-content">
+                        <li><a href="#">intro 1</a></li>
+                        <li><a href="#">intro 2</a></li>
+                    </ul>
+                </li>
+            </div>
+            <div>
+                <li class="li-p">
+                    <a href="#" class="dropbtn" onclick="opc4()">Opc 4
+                        <span class="fas fa-caret-down"></span>
+                    </a>
+                    <ul id="myDropdown4" class="dropdown-content">
+                        <li><a href="#">intro 1</a></li>
+                        <li><a href="#">intro 2</a></li>
+                    </ul>
+                </li>
+            </div>
+            
+        </ul>
+    </nav>
+
+    <script></script>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="/css/app.css">
+    <script src="/js/app.js"></script>
+
+    <title>Menu</title>
+    @include('partials.styles')
+
+</head>
+
+<body>
+    @yield('menu')
+
+    @include('partials.scripts')
+</body>
+
+</html>
