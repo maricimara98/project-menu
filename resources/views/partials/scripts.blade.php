031837f1c1d5c119ae8cba50c26279b08a76a70d
Jucimara Pereira <maricimara98@outlook.com>
2022-11-04 12:07:46 -0300

    scripts e styles das páginas

---
+++ resources/views/partials/scripts.blade.php
@@ -0,0 +1,1 @@
+<script src="https://kit.fontawesome.com/f4ecad88e2.js" crossorigin="anonymous"></script>
---
+++ resources/views/partials/styles.blade.php
@@ -0,0 +1,4 @@
+<!-- Fontes -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins&family=Rubik:ital@1&family=Ubuntu:wght@700&display=swap" rel="stylesheet">
