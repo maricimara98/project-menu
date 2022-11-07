c7c3b96046b0ed95062131c703a926d2e6e7dfe9
Jucimara Pereira <maricimara98@outlook.com>
2022-11-07 11:52:54 -0300

    alteração no script

--- resources/views/layouts/scripts.blade.php
+++ resources/views/layouts/scripts.blade.php
@@ -1,7 +1,6 @@
 <!-- Scripts -->
-<script src="https://estacionamento.huufma.br/static/assets/vendor/modernizr/modernizr.js"></script>
-<script src="https://estacionamento.huufma.br/static/assets/vendor/breakpoints/breakpoints.js"></script>
-<script src="https://estacionamento.huufma.br/static/assets/vendor/breakpoints/breakpoints.js"></script>
-<script src="https://estacionamento.huufma.br/static/assets/vendor/moment/moment.min.js"></script>
-<script src="https://estacionamento.huufma.br/static/assets/vendor/moment/moment-with-locales.min.js" type="text/javascript"></script>
-<script src="https://estacionamento.huufma.br/static/assets/vendor/bootstrap-sweetalert/sweet-alert.min.js"></script>
+<script src="js/modernizr/modernizr.js"></script>
+<script src="js/breakpoints/breakpoints.jss"></script>
+<script src="js/moment/moment.min.js"></script>
+<script src="js/moment/moment-with-locales.min.js"></script>
+<script src="js/bootstrap-sweetalert/sweet-alert.min.js"></script>
